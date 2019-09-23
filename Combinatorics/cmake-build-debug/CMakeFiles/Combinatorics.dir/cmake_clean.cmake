95144ebc6bc25896766d6340b49ea3248eea5c99
C0ff33C0d3r <36044777+C0ff33C0d3r@users.noreply.github.com>
2019-09-24 01:09:23 +0300

    Combinatorics

---
+++ Combinatorics/cmake-build-debug/CMakeFiles/Combinatorics.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Combinatorics.dir/main.cpp.obj"
+  "Combinatorics.pdb"
+  "Combinatorics.exe"
+  "Combinatorics.exe.manifest"
+  "libCombinatorics.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Combinatorics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
