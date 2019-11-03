9dd82662d8764ba64f94c6b83fe0c3fb2a01654b
Alexey Volkov <volkov.aa@phystech.edu>
2019-11-03 19:46:14 +0300

    Added list constructor

--- DoublyLinkedListedClassic/main.cpp
+++ DoublyLinkedListedClassic/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cstdlib>
 
 struct node_t {
     node_t *next;
@@ -9,10 +10,19 @@ struct node_t {
 struct list_t {
     node_t *head;
     node_t *tail;
-    size_t elems;
+    size_t size;
 };
 
 int main() {
 
     return 0;
+}
+
+list_t *createList() {
+    list_t *newList = (list_t *) calloc(1, sizeof(list_t));
+    newList->size = 0;
+    newList->head = nullptr;
+    newList->tail = nullptr;
+
+    return newList;
 }
